e1b2ca0362ffdeee3dbad20e14f128437078b218
Quangha Ha <lecongthinh2005@gmail.com>
2026-05-19 16:03:47 +0700

    update

---
+++ resources/views/demoindex2.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    {{--Cu phap PHP thuan--}}
+    <h1>Xin chao ban den voi he thong <?=$data ?></h1>
+    <hr>
+{{-- Cu phap Blade Template (Laravel) --}}
+    <h1>Xin chao ban den voi he thong {{$data }}</h1>
+</body>
+</html>
